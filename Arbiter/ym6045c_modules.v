cd787c6d429831cb7fec97326e67542fd3a9e480
nukeykt <alexeytf2@gmail.com>
2024-04-16 00:45:41 +0900

    ym6045c modules (based on fc1004)

--- Arbiter/ym6045c_modules.v
+++ Arbiter/ym6045c_modules.v
@@ -1,16 +1,9 @@
 // TODO:
-// ym6045c_cell_14_cnt_bit
-// ym6045c_cell_9_dffs
-// ym6045c_cell_31
 // ym6045c_cell_32
 // ym6045c_cell_27_delay_short
 // ym6045c_cell_33_delay_med
 // ym6045c_cell_34_delay_long
-// ym6045c_cell_23_dff
-// ym6045c_cell_2_dffr
 // ym6045c_cell_35
-// ym6045c_cell_17_sr8
-// ym6045c_cell_38
 //
 
 // Module Definitions [It is possible to wrap here on your primitives]
@@ -303,3 +296,178 @@ module ym6045c_cell_37_nor8 (  i1, i2, i3, i4, o5, i6, i7, i8, i9);
 
 endmodule // ym6045c_cell_37_nor8
 
+module ym6045c_cell_14_cnt_bit (  o2, o3, i6, i7, i13, i14, o15, i18);
+
+	output wire o2;
+	output wire o3;
+	input wire i6;
+	input wire i7;
+	input wire i13;
+	input wire i14;
+	output wire o15;
+	input wire i18;
+	
+	reg data;
+	
+	wire s = data ^ i18;
+	assign o15 = data & i18;
+	
+	assign o2 = data;
+	assign o3 = ~data;
+	
+	always @(posedge i6, negedge i7)
+	begin
+		if (~i7)
+			data <= 1'h0;
+		else if (i14)
+			data <= i13;
+		else
+			data <= s;
+	end
+
+endmodule // ym6045c_cell_14_cnt_bit
+
+module ym6045c_cell_9_dffs (  i3, i4, i7, o11, o13);
+
+	input wire i3;
+	input wire i4;
+	input wire i7;
+	output wire o11;
+	output wire o13;
+	
+	reg data;
+	
+	assign o13 = data;
+	assign o11 = ~data;
+	
+	always @(posedge i3, negedge i7)
+	begin
+		if (~i7)
+			data <= 1'h1;
+		else
+			data <= i4;
+	end
+
+endmodule // ym6045c_cell_9_dffs
+
+module ym6045c_cell_2_dffr (  o2, o4, i9, i11, i13);
+
+	output wire o2;
+	output wire o4;
+	input wire i9;
+	input wire i11;
+	input wire i13;
+	
+	reg data;
+	
+	assign o13 = data;
+	assign o11 = ~data;
+	
+	always @(posedge i13, negedge i9)
+	begin
+		if (~i7)
+			data <= 1'h0;
+		else
+			data <= i11;
+	end
+
+endmodule // ym6045c_cell_2_dffr
+
+module ym6045c_cell_23_dff (  o2, o5, i10, i11);
+
+	output wire o2;
+	output wire o5;
+	input wire i10;
+	input wire i11;
+	
+	reg data;
+	
+	assign o5 = data;
+	assign o2 = ~data;
+	
+	always @(posedge i11)
+	begin
+		data <= i10;
+	end
+
+endmodule // ym6045c_cell_23_dff
+
+module ym6045c_cell_17_sr8 (  i2, o3, o10, o16, o23, o29, o36, o42, o49, i56, i57);
+
+	input wire i2;
+	output wire o3;
+	output wire o10;
+	output wire o16;
+	output wire o23;
+	output wire o29;
+	output wire o36;
+	output wire o42;
+	output wire o49;
+	input wire i56;
+	input wire i57;
+	
+	reg [7:0] data;
+	
+	assign o3 = data[0];
+	assign o10 = data[1];
+	assign o16 = data[2];
+	assign o23 = data[3];
+	assign o29 = data[4];
+	assign o36 = data[5];
+	assign o42 = data[6];
+	assign o49 = data[7];
+	
+	always @(posedge i57, negedge i2)
+	begin
+		if (~i7)
+			data <= 8'h0;
+		else
+			data <= { i56, data[7:1] };
+	end
+
+endmodule // ym6045c_cell_17_sr8
+
+module ym6045c_cell_38 (  o1, o2, i10, i12);
+
+	output wire o1;
+	output wire o2;
+	input wire i10;
+	input wire i12;
+	
+	reg data;
+	
+	assign o1 = data;
+	assign o2 = ~data;
+	
+	always @(posedge i12, negedge i10)
+	begin
+		if (~i10)
+			data <= 1'h0;
+		else
+			data <= ~data;
+	end
+
+endmodule // ym6045c_cell_38
+
+module ym6045c_cell_31 (  i2, i3, i4, i13, o16, o17);
+
+	input wire i2;
+	input wire i3;
+	input wire i4;
+	input wire i13;
+	output wire o16;
+	output wire o17;
+	
+	reg data;
+	assign o16 = data;
+	assign o17 = ~data;
+	
+	always @(negedge i2, negedge i13)
+	begin
+		if (~i13)
+			data <= 1'h0;
+		else
+			data <= ~(~(i3 | data) | (i4 & data));
+	end
+
+endmodule // ym6045c_cell_31
